7a0518ba329671e4404bb417ddef752b40aa4c84
Liu Haibin <liu.haibin@zumata.com>
2015-09-11 14:47:26 +0800

    Add form validation

---
+++ static/app.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+  'use strict';
+  var emailReg = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w+)+$/;
+  $('#submitButton').click(function(e) {
+    if ($('#inputName').val() === '') {
+      e.preventDefault();
+      $('#inputNameFormGroup').addClass('has-error');
+    } else if ($('#inputEmail').val() === '') {
+      e.preventDefault();
+      $('#inputEmailFormGroup').addClass('has-error');
+    } else if (!emailReg.test($('#inputEmail').val())) {
+      e.preventDefault();
+      $('#inputEmailFormGroup').addClass('has-error');
+    } else if ($('#inputPassword').val() === '') {
+      e.preventDefault();
+      $('#inputPasswordFormGroup').addClass('has-error');
+    } else if ($('#inputRepeatPassword').val() === '') {
+      e.preventDefault();
+      $('#inputRepeatPasswordFormGroup').addClass('has-error');
+    } else if ($('#inputPassword').val() !== $('#inputRepeatPassword').val()) {
+      e.preventDefault();
+      $('#inputPasswordFormGroup').addClass('has-error');
+      $('#inputRepeatPasswordFormGroup').addClass('has-error');
+    } else {
+      $('#signupForm').submit();
+    }
+  });
+});
